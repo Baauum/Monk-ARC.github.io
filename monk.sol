5db344cd5e5800c2b6a1e9d35537ef7db4832d9b
Baauum <123036397+Baauum@users.noreply.github.com>
2025-11-19 14:26:05 +1000

    Update monk.sol

--- monk.sol
+++ monk.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+// GitHub imports for Remix
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/security/ReentrancyGuard.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/access/Ownable.sol";
 
 /**
  * @title RaffleLendingProtocol
@@ -72,7 +73,7 @@ contract RaffleLendingProtocol is ReentrancyGuard, Ownable {
     event RaffleCreated(uint256 indexed raffleId, uint256 rewardPool, uint256 endTime);
     event RaffleDrawn(uint256 indexed raffleId, address[] winners, uint256 rewardPerWinner);
     
-    constructor() {}
+    constructor() Ownable(msg.sender) {}
     
     // Admin functions
     function createPool(
